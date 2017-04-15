95e0238c5d0bad1cb2c3854f1b5f4d5eed441161
mugenyi henry <kraytosda1st@gmail.com>
2017-04-15 12:29:12 +0300

    remove check for request in progress

--- www/plugin.js
+++ www/plugin.js
@@ -4,21 +4,16 @@ var PLUGIN_NAME = 'WriteSettings';
 
 var WriteSettings = {
     requestWriteSettings: function(successCallback, errorCallback) {
-        if (requestInProgress) {
-            return onError("A runtime permissions request is already in progress");
-        }
 
         function onError(error) {
-            requestInProgress = false;
             errorCallback(error);
         }
 
         function onSuccess(status) {
-            requestInProgress = false;
             successCallback(status);
         }
 
-        requestInProgress = true;
+
 
         exec(onSuccess, onError, PLUGIN_NAME, 'requestPermission', []);
     }
